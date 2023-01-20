f55b8e46a785d642307ed3ff20eaf17b99120776
arunkumaran1099 <arunkumaran1099@gmail.com>
2023-01-20 14:32:22 +0530

    code upload

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+<form method="POST" action="/insert">
+    @csrf
+    <label>username</label>
+    <input type="text" name="name"></br>
+    <label>password</label>
+    <input type="password" name="password"></br>
+    <input type="submit" name="submit"></br>
+</form>
---
+++ routes/web.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return 'hello';
+// });
+
+// Route::match (['get', 'post'], '/', function () {
+//     return view('home');
+
+// });
+
+// Route::get('/', function (request $request) {
+//     $name = $request->input('name');
+//     $pas= $request->input('password');
+
+//     dd($name);
+
+// });
+
+// Route::get('/', function () {
+//      return 'hello';
+// });
+
+
+// ---------------------------------------------------------------------------
+// Route::redirect('/', 'home');
+
+// Route::get('home', function () {
+//     return view('home');
+// });
+// ----------------------------------------------------------------------------
+
+// Route::view('/', 'home');
+
+// Route::get('/user/{id}', function (Request $request, $id) {
+//     return 'user' . $id;
+// });
+
+// Route::get('/user/{name?}', function ($name = 'john') {
+//      return 'user ' . $name;
+//     //  --return user john
+// });
